a92c8e494285190674bbfc904b44a72ea9107e44
aravindan2020 <61485230+aravindan2020@users.noreply.github.com>
2024-02-03 13:51:32 +0530

    Add files via upload

---
+++ Addtion.java
@@ -0,0 +1,11 @@
+package Arithmetic1;
+
+public class Addtion {
+	
+	    public static void main(String[] args) {
+		int a=12,b=8;
+		int c=a+b;
+		System.out.println("the addition value is "+c);
+	}
+
+}
---
+++ Arithmet.java
@@ -0,0 +1,37 @@
+package Arithmetic1;
+
+public class Arithmet {
+	public static void Add()
+	{
+		int a=8,b=13;
+		int c=a+b;
+		System.out.println(c);		
+	}
+	public static void Sub()
+	{
+		int d=8,e=5;
+		int f=d-e;
+		System.out.println(f);		
+	}
+	public static void Mul()
+	{
+		int g=8,h=5;
+		int i=g*h;
+		System.out.println(i);		
+	}
+	public static void Div()
+	{
+		int j=8,k=2;
+		int s=j/k;
+		System.out.println(s);		
+	}
+
+	public static void main(String[] args) {
+		Arithmet.Add();
+		Arithmet.Sub();
+		Arithmet.Mul();
+		Arithmet.Div();
+		
+	}
+
+}
