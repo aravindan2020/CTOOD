7be5cfa34b771d856b89406456a0a0a324e573fa
aravindan2020 <61485230+aravindan2020@users.noreply.github.com>
2024-02-03 13:51:59 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,13 @@
+package Addition;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a=9,b=10;
+		int c=a+b;
+		System.out.println("the valus of c is"+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
