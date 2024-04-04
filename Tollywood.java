985681654cc4592859ebd8a5b3c5d3a4fa2f23de
aravindan2020 <61485230+aravindan2020@users.noreply.github.com>
2024-04-04 15:11:30 +0530

    Add files via upload

---
+++ Tollywood.java
@@ -0,0 +1,22 @@
+
+public class Tollywood {
+	double money;
+
+	public Tollywood(double money) {
+		this.money = money;
+	}
+	void profit()
+	{
+		System.out.println("boxoffice collection");
+	}
+void display()
+{
+}
+	@Override
+	public String toString() {
+		return "Tollywood [money=" + money + "]";
+	}
+	
+	
+
+}
